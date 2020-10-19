f7d0a477024496d4f2034a0234415768a46e83e4
JohaGL <deisy.lascroux@etu.univ-lyon1.fr>
2020-10-19 19:17:24 +0200

    still same error

--- flasktest/routes.py
+++ flasktest/routes.py
@@ -31,30 +31,31 @@ def home():
 @app.route('/mailconfirm', methods=['GET','POST'])
 ## https://www.youtube.com/watch?v=vF9n248M1yk
 def mailconfirm():
-	email = request.form['email']
+	email = request.form ['email']
 	token = serialz.dumps(email, salt='email-confirm')	
 	link = url_for('mailconfirm', token=token, _external=True)
 	msg = Message("Confirm your mail", sender=sendermail, recipients=[email])
 	msg.body = 'Click on this link to start using bpp-web : {}'.format(link)
 	mail.send(msg) 
-	return '<p> GO TO YOUR MAIL AND DO AS WE COMMAND YOU ;)!!! <br> input mail is :\
-		 {}<br>token : {}</p>'.format(email, token)
+	return '<p> GO TO YOUR MAIL AND DO AS WE COMMAND YOU ;)\
+		 **check also spam**!!! <br> input mail is : {}\
+		 <br>token : {}</p>'.format(email, token)
 
 @app.route("/mailconfirmEnd/<token>")
 def mailconfirmEnd(token):
 	try:
-		email = serialz.loads(token, salt="email-confirm", max_age=3600)
-		return f'<h1>yeah::::{token}::::{email}</h1>'
-		# return redirect(url_for('querry'))  #TODO tomorrow check if redirect is ok
+		outm = serialz.loads(token, salt="email-confirm", max_age=3600)
+		#return f'<h1>yeah, {token}, mail : {outm}</h1>'
+		return redirect(url_for('query'))  #TODO tomorrow check if redirect is ok
 	except SignatureExpired:
-		return f'<h2> The token is expired ::::{token}:::{email}</h2>'
+		return f'<h2> The token {token} is expired {outm}</h2>'
 		
 	
 
-@app.route('/querry', methods=['GET', 'POST'])
-def querry():
+@app.route('/query', methods=['GET', 'POST'])
+def query():
 	form1 = testForm()
-	return render_template('querry.html', title = 'querry', form=form1)
+	return render_template('query.html', title = 'query', form=form1)
     
 @app.route('/results')
 def results():
